0716ecd66f2eb6d19c2aa59756ca94e97646f82c
Luis Starlino <luis.guilherme009@gmail.com>
2021-06-11 08:41:05 -0300

    Using all the For's

--- 04-lists/for-forIN/01-for_forIn_forOf.js
+++ 04-lists/for-forIN/01-for_forIn_forOf.js
@@ -6,14 +6,29 @@ async function main(){
         const names = [];
         
         //USING FOR
+        
         console.time("Use for->")
         for(let i = 0; i<= resut.length -1 ; i++ ){
             const turnUser =  resut[i].name;
             names.push(turnUser);
         }
         console.timeEnd("Use for->")
-
-
+        
+       //USING FOR I
+       
+       console.time("Use for I->");
+       for(let i in resut){
+        const turnUser =  resut[i].name;
+        names.push(turnUser);
+       }
+       console.timeEnd("Use for I->");
+       
+      //USING FOR X IN X'S
+      console.time("For in->");
+      for(user in resut){
+        names.push(resut[user].name);
+      }
+      console.timeEnd("For in->");
     } catch (error) {
         console.log("Your error message-->", error.message);
     }
